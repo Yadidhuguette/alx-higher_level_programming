b81392a5ffc01f9b6c18ed6314a89eaafa7dc696
Huguette <yadidmishkan@gmail.com>
2023-01-22 03:38:27 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """that fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 
+
 if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
         content = page.read()
