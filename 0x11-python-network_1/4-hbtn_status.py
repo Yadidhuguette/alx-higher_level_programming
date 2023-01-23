3b43a55af7d4a8df3eb6676dd3ab9a49ababc4c6
Huguette <yadidmishkan@gmail.com>
2023-01-23 04:50:15 -0800

    Task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status."""
-
 import requests
 
+
 if __name__ == "__main__":
-    content = requests.fetch("https://alx-intranet.hbtn.io/status").text
+    content = requests.get("https://alx-intranet.hbtn.io/status").text
     print("body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
