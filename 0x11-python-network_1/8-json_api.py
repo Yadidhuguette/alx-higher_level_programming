84c757681c957aa5c7660e796a0db0a8b16f284b
Huguette <yadidmishkan@gmail.com>
2023-01-24 20:43:41 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to the URL
+with the letter as a parameter."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    data = {"q": letter}
+
+    request = requests.post("http://0.0.0.0:5000/search_user", data)
+    try:
+        response == request.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except:
+        print("Not a valid JSON")
