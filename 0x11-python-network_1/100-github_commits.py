326cc67a5859e074ea8e9123e1ba44db9fd59b04
Huguette <yadidmishkan@gmail.com>
2023-01-26 03:34:24 -0800

    Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""takes 2 arguments in order to solve this challenge like
-list 10 commits of repository "rails" by user "rails"."""
-
-from sys import argv
+"""list 10 commits of repository "rails" by user "rails"."""
 import requests
+import sys
+
 
 if __name__ == "__main__":
-    URL = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
-            .format(argv[2], argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
 
-            r = requests.get(URL)
-            commits = r.json()
-            for commit in commits:
-                print("{}: {}".format(commit.get("sha"),
-                                      commit.get("commit").get("author").get("name")))
+    request = requests.get9url)
+    commits = request.json()
+    try:
+        for a in range(10):
+            print("{}: {}".format(
+                commits[a].get("sha"),
+                commits[a].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
